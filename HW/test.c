4a827ec66e7037dfb20a5272ed35b9fff86b585b
weiiiii0622 <weiiiii.0622@gmail.com>
2022-02-17 17:12:51 +0800

    update HW0

--- HW/test.c
+++ HW/test.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "function.h"
+#include <string.h>
+#define ll long long
+#define swap(x,y) {int temp=x; x=y; y=temp;}
 
-void deleteNode(Node ** nd, int data){
-    int count=0;
-    Node *cur=*nd;
-    Node *before=NULL;
-    while(cur != NULL){
-        count++;
-        if(count==data){
-            if(before!=NULL) before->next = cur->next;
-            else cur = cur->next;
-            free(cur);
-            break;
-        }
-        before = cur;
-        cur = cur->next;
-    }
-    return;
+int dir[8][2] = {{1,2}, {2,1}, {-1,2}, {2,-1}, {1,-2}, {-2,1}, {-1,-2}, {-2,-1}};
+char board[100][100];
+char str[12] = "ICPCASIASG\0";
+int done=0;
+int n;
+
+int check(int x, int y, int target){
+    if(x>=0&&x<n&&y>=0&&y<n&&board[x][y]==str[target]) return 1;
+    return 0;
 }
 
-Node* createList(){
-    Node *hd=NULL;
-    Node *cur;
-    int data;
-    while (scanf("%d", &data) != EOF){
-        if(data==-1) break;
-        Node *new = (Node*)malloc(sizeof(Node));
-        new->data = data;
-        new->next = NULL;
-        if(hd==NULL){
-            hd = new;
-            cur = hd;
-        }
-        else{
-            cur->next = new;
-            cur = cur->next;
+void solve(int x, int y, int target){
+    if(target==10){
+        done = 1;
+        return;
+    }
+    for(int i=0; i<8; i++){
+        if(check(x+dir[i][0], y+dir[i][1], target) && !done){
+            solve(x+dir[i][0], y+dir[i][1], target+1);
         }
     }
-    return hd;
+} 
+
+int main(){
+    scanf("%d", &n);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            scanf(" %c", &board[i][j]);
+        }
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(board[i][j] == 'I' && !done){
+                solve(i, j, 1);
+            }
+        }
+    }
+    printf("%s\n", done?"YES":"NO");
+    return 0;
 }
